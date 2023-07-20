722e82d4c4b9e6d0f06c1200d63d2bd08aae3c9e
T. Paul Kariuki <tpaulkariuki@gmail.com>
2023-07-20 10:58:35 -0500

    migrated functions

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 """Main entry point for Network Tools"""
 
+from network.__utils__ import config_file_check
+from network.site_config import experiment
+
+
 try:
     import consolemenu
+    import cutie
     from contextlib import suppress
     from blessed.win_terminal import Terminal
     from network.__utils__ import clear_screen
@@ -20,54 +25,35 @@ choices = [
     "Generate AP SN/MAC spreadsheet template",
     "Merge fragmented spreadsheets",
     "Wipe a Cisco ASA Firewall",
+    term.gray30("Requires valid firewall credentials to automatically log in"),
 ]
 
 
-prompt_args = lambda: consolemenu.SelectionMenu.get_selection(
-    choices,
-    "Welcome to the Network Tools utility!\n\n\nUse any of these tools to assist in configuring network equipment. To exit any of the tools during execution, use Ctrl+C.",
-    "Please select an option from the list:",
-)
+# prompt_args = lambda: consolemenu.SelectionMenu.get_selection(
+#     choices,
+#     "Welcome to the Network Tools utility!\n\n\nUse any of these tools to assist in configuring network equipment. To exit any of the tools during execution, use Ctrl+C.",
+#     "Please select an option from the list:",
+# )
 
 
-def config_file_check():
-    from network.__utils__ import get_config_filename
-    from network.site_config import SiteConfiguration
-
-    cont = True
-    while cont:
-        config = SiteConfiguration(get_config_filename("Enter name of config file:"))
-        rng, gaps = config.get_range_string(True, True)
-        print(f"\nIncluded configs:\t{rng}\nGaps in configs:\t{gaps or 'None'}")
-        with term.cbreak():
-            print("\n\nCheck another file? [y/N]:  ", end="", flush=True)
-            cont = (key := str(term.inkey()).lower()) not in [
-                "n",
-                "no",
-                "\r",
-                "\n",
-                "",
-                " ",
-            ]
-        clear_screen()
-
-
-def experiment():
-    from network.__utils__ import get_config_filename, clear_screen
-    from network.site_config import SiteConfiguration
-    import code
-
-    def console_exit():
-        raise SystemExit
-
-    config = SiteConfiguration(get_config_filename("Enter name of config file:"))
+def prompt_args() -> int:
     clear_screen()
-    banner = f"""This interactive Python terminal will allow you to experiment with a SiteConfiguration object.\n\nThe object has been generated from the file you chose:\n\t{config.filepath}\nand you can access the methods/functions and attributes for that object using the 'config' variable.\n\nFor help on how to interact with object, use help(config).\n\nTo exit the interactive terminal, use exit()."""
-    exitmsg = """"""
-    shell = code.InteractiveConsole(
-        {**globals(), **locals(), "config": config, "exit": console_exit}
+    print(
+        term.bold_crimson("\nWelcome to the Network Tools utility!"),
+        "\tUse any of these tools to assist in working with network equipment.",
+        term.coral("\tTo exit any of the tools during execution, ")
+        + term.bold_coral("use Ctrl+C."),
+        sep="\n",
+        end="\n\n",
     )
-    shell.interact(banner=banner, exitmsg=exitmsg)
+    print(
+        term.yellow("Please select an option from the list:"),
+        "(use ↑/↓ + ENTER to select)",
+        end="\n\n",
+    )
+    ch = cutie.select(choices, [6], caption_prefix="    * ")
+    clear_screen()
+    return ch
 
 
 if __name__ == "__main__":
